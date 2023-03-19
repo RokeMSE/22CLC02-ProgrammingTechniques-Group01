85de1cced2bfb1eb91cca0422425a404c24b31cd
ketamean <truongthanhtoan2003@gmail.com>
2023-03-19 19:22:42 +0700

    suggesting structs definition
    suggest a way to define all user-defined datatypes used in the system

---
+++ StructDefinition/CLASS.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define uint unsigned int
+#define ushort unsigned short
+
+struct CLASS {
+    uint K;
+    Program program;
+    ushort No;
+    // 22CLC2: K = 22, program = CLC, No = 2
+};
