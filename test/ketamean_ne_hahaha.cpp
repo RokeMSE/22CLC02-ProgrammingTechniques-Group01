06db04978b725f4224be59397919202892e10f70
ketamean <120916278+ketamean@users.noreply.github.com>
2023-03-19 18:50:15 +0700

    delete testing commit

--- test/ketamean_ne_hahaha.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout << "ketamean la ai ma sao dep trai qua z???" << endl;
-    cout << "\t\t\"chuc 10d\" - by Ho Tuan Thanh";
-    return 0;
-}
