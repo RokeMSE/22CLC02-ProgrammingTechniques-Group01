c165f0b42a853a324def00e1ed8729098cd52682
ketamean <120916278+ketamean@users.noreply.github.com>
2023-03-22 15:23:49 +0700

    Update template of struct LIST
    Avoid using the key word `class`

--- StructDefinition_update/LIST.h
+++ StructDefinition_update/LIST.h
@@ -10,8 +10,7 @@ struct SLL {
     data_type data;
     SLL<data_type>* next = nullptr;
 };
-/////////////////////////////////////////////////
-// _____________________________________________
+// // // _____________________________________________
 
 /////////////////////////////////////////////////
 template <typename data_type> 
@@ -20,11 +19,10 @@ struct DLL {
     data_type data;
     DLL<data_type>* prev = nullptr, *next = nullptr;
 };
-// /////////////////////////////////////////////////
 // // // _____________________________________________
 
 /////////////////////////////////////////////////
-template <template <typename> class list_type, typename Data_Type>
+template <template <typename> typename list_type, typename Data_Type>
 struct LIST {
     list_type<Data_Type> *head, *tail;
         // list_type:   SLL or DLL
@@ -36,27 +34,4 @@ struct LIST {
     void insert (list_type<Data_Type>* node);
     void remove (list_type<Data_Type>* node);
 };
-
-//LUONG
-// template <typename list_type>
-// struct LIST {
-//     //using typename data_type;
-//     //list_type<typename list_type::data_type> *head, *tail;
-//     //template <typename data_type> 
-//     list_type* head, *tail;
-//     //list_type::list_type<data_type>* head;
-//     //list_type <data_type>* head, *tail;
-//     //list_type <type>* head, *tail; 
-//         // list_type:   SLL or DLL
-//         // data_type:   data type of member `data` in DLL or SLL
-//         // example, to declare a DLL list of students: LIST<DLL<STUDENT>>
-
-
-//     // member functions
-//     // those prototypes haven't been finished yet
-//     void insert (list_type* node);
-//     void remove (list_type* node);
-// };
-
-/////////////////////////////////////////////////
-// _____________________________________________
+// // // _____________________________________________
