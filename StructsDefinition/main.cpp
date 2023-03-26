3b18d8b27da2937920a19408a97f30792de3167d
DrDATNguyen <102152763+DrDATNguyen@users.noreply.github.com>
2023-03-26 12:24:18 +0700

    update

--- StructsDefinition/main.cpp
+++ StructsDefinition/main.cpp
@@ -2,16 +2,22 @@
 #include<iostream>
 using namespace std;
 int main() {
-   LIST<SLL, STUDENT> student;
-   //STUDENT student;
+    LIST<STUDENT> ST;
+    LIST<STUDENT> list;
+    STUDENT student;
+    //STUDENT student;
     int soluongSV = 0;
     int idCount = 0;
     int key;
-    while (true) {
+    do {
         cout << "CHUONG TRINH QUAN LY SINH VIEN C/C++\n";
         cout << "*************************MENU**************************\n";
         cout << "**  1. Them sinh vien.                               **\n";
-        cout << "**  2. Cap nhat thong tin sinh vien boi ID.          **\n";
+        cout << "**  2. Xuat         **\n";
+        cout << "**  3. Export to teacher **\n";
+        cout << "**  4. Read file from teacher **\n";
+        cout << "**  5. Update list student         **\n";
+        cout << "**  6. out.          **\n";
         cout << "*******************************************************\n";
         cout << "Nhap tuy chon: ";
         cin >> key;
@@ -19,20 +25,40 @@ int main() {
         case 1:
             cout << "\n1. Them sinh vien.";
             idCount++;
-            import(student);
+            ST.import(ST, student);
             cout << "\nThem sinh vien thanh cong!";
             soluongSV++;
-           // pressAnyKey();
+            //pressAnyKey();
             break;
         case 2:
             if (soluongSV > 0) {
-                cout << "\n7. Hien thi danh sach sinh vien.";
-                Export(student);
+                cout << "\nHien thi danh sach sinh vien.";
+
+                ST.Export(ST);
             }
             else {
                 cout << "\nSanh sach sinh vien trong!";
             }
-           // pressAnyKey();
+            // pressAnyKey();
+            break;
+        case 3:
+
+            EXportToTeacher();
+
+            // pressAnyKey();
+            break;
+        case 4:
+
+            ReadFileFromTeacher(list);
+            list.Export(list);
+
+            // pressAnyKey();
+            break;
+        case 5:
+
+            UpdateST(list, ST);
+            //ST.Export(ST);
+            // pressAnyKey();
             break;
         default:
             cout << "\nKhong co chuc nang nay!";
@@ -40,5 +66,5 @@ int main() {
             //pressAnyKey();
             break;
         }
-    }
+    } while (key != 6);
 }
