a3794e30da69bca508283f73a22a12badc4959df
DrDATNguyen <102152763+DrDATNguyen@users.noreply.github.com>
2023-03-27 16:00:37 +0700

    update

---
+++ header.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define uint unsigned int
+#define ushort unsigned short
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstring>
+#pragma warning(disable : 4996)
+using namespace std;
+// Structs 
+#include "StructsDefinition/enum.h"
+#include "StructsDefinition/LIST.h"
+
+#include "StructsDefinition/Stack.h"
+#include "StructsDefinition/Queue.h"
+
+#include "StructsDefinition/USER.h"
+#include "StructsDefinition/STUDENT.h"
+#include "StructsDefinition/STAFF.h"
+#include "StructsDefinition/CLASS.h"
+#include "StructsDefinition/COURSE.h"
+#include "StructsDefinition/DATE.h"
+#include "StructsDefinition/SCHOOLYEAR.h"
+/////////////////////////////////////
+
+// Global variables
+SCHOOLYEAR g_currentSchoolYear; // g stands for global
+SEMESTER g_currentSemester;
+/////////////////////////////////////
+
+using namespace std;
+
+// GUI
+
+/////////////////////////////////////
+
+// Prototypes
+
+/////////////////////////////////////
