eeb4a3d5a1b29a4676111a9b4e919b1213884ccb
nxt964 <thanhnx964@gmail.com>
2023-03-30 22:57:44 +0700

    Update COURSE.cpp

--- Implementation/COURSE.cpp
+++ Implementation/COURSE.cpp
@@ -34,31 +34,23 @@ bool COURSE::addStudents (std::string filename) {
     //  The format of file csv: No,Student ID,First Name,Last Name,Gender,Date Of Birth,Social ID
     while (!ifs.eof())
     {
-        STUDENT stu;
 
         getline(ifs,str,',');       //No
-        stu.No = stoi(str);
-
         getline(ifs,str,',');       //Student ID
-        stu.studentID = str;
 
-        getline(ifs,str,',');       //First Name
-        stu.firstname = str;         
+        // find this student by studentID in the global list of students
+        DLL<STUDENT*> *cur = L_Student.head;
+        while (cur != nullptr && cur->data->studentID != str)
+            cur = cur -> next;
 
-        getline(ifs,str,',');       //Last Name
-        stu.lastname = str;
-
-        getline(ifs,str,',');       //Gender
-        if (str == "Nam")   stu.gender = 0; //nam
-        else    stu.gender = 1; //nu
-
-        getline(ifs,str,',');       //Date of Birth
-        stu.DoB = getDate(str);
-
-        getline(ifs,str);           //Social ID (at the last => getline)
-        stu.socialID = str;
-
-        add1Student(&stu);       // Add students to the list of students in the course
+        if (cur == nullptr)
+        {
+            cout << "Can not find student: " << str << endl;
+            return false;
+        }
+        else
+            add1Student(cur->data);       // Add students to the list of students in the course
+        getline(ifs,str);       // next line
     }
     ifs.close();
     return true;
