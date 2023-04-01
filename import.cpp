d92c123d7f179e0e2c2b367454a2c7c702ff8e81
DrDATNguyen <102152763+DrDATNguyen@users.noreply.github.com>
2023-04-01 22:59:35 +0700

    update

--- import.cpp
+++ import.cpp
@@ -1,11 +1,22 @@
-#include <fstream>
+﻿#include <fstream>
 #include "header.h"
 using namespace std;
 
+bool stringToBool(std::string str)//ham doi tu string -> bool
+{
+    if (str == "true" || str == "1") {
+        return true;
+    }
+    else if (str == "false" || str == "0") {
+        return false;
+    }
+    else {
+        throw std::invalid_argument("Invalid input string");
+    }
+}
 
-bool importStudents(std::string filename) {
+bool importStudents() {
     ifstream ifs("CSV/Student.csv");
-    ifs.open(filename);
     if (ifs.is_open() == false)
         return false;
     string str;
@@ -13,15 +24,14 @@ bool importStudents(std::string filename) {
     while (getline(ifs, str)) {
         count++;
     }
-    //LIST<DLL, STUDENT*> L_Student
     getline(ifs, str);
     while (!ifs.eof())
     {
         for (int i = 0; i < count; i++) {
             STUDENT* tmp = new STUDENT;
             getline(ifs, str, ','); // get username
-            
             tmp->username = str;
+
             getline(ifs, str, ','); // password
             
             tmp->password = str;
@@ -38,6 +48,7 @@ bool importStudents(std::string filename) {
             
             tmp->lastname = str;
             getline(ifs, str, ',');
+            tmp->gender = stringToBool(str);
             
             getline(ifs, str, ',');
             
@@ -56,18 +67,93 @@ bool importStudents(std::string filename) {
             }
             L_Student.tail->data = tmp;
             L_Student.tail->next = nullptr;
-            //cur = cur->next;
         }
         break;
     }
 }
 
-bool importStaffs(std::string filename) {
+bool importStaffs() {
+    ifstream ifs("CSV/Staff.csv");
+    if (ifs.is_open() == false)
+        return false;
+    string str;
+    int count = 0;
+    while (getline(ifs, str)) {
+        count++;
+    }
+    getline(ifs, str);
+    while (!ifs.eof())
+    {
+        for (int i = 0; i < count; i++) {
+            STAFF* tmp = new STAFF;
+            getline(ifs, str, ','); // get username
+            tmp->username = str;
 
+            getline(ifs, str, ','); // password
+            tmp->password = str;
+
+            getline(ifs, str, ','); 
+            tmp->firstname = str;
+
+            getline(ifs, str, ',');
+            tmp->lastname = str;
+
+           
+            if (L_Staff.head == nullptr) L_Staff.head = L_Staff.tail = new DLL<STAFF>;
+            else {
+                L_Staff.tail->next = new DLL<STAFF>;
+                L_Staff.tail->next->prev = L_Staff.tail->next;
+                L_Staff.tail = L_Staff.tail->next;
+            }
+            L_Staff.tail->data = tmp;
+            L_Staff.tail->next = nullptr;
+            
+        }
+        break;
+    }
 }
 
-bool importClasses(std::string filename) {
 
+bool importClasses(std::string filename) {
+    ifstream ifs("CSV/Class.csv");
+    if (ifs.is_open() == false)
+        return false;
+    string str;
+    int count = 0;
+    while (getline(ifs, str)) {
+        count++;
+    }
+    getline(ifs, str);
+    while (!ifs.eof())
+    {
+        for (int i = 0; i < count; i++) {
+            CLASS* tmp = new CLASS;
+            getline(ifs, str, ',');
+            unsigned short n = static_cast short > (std::stoul(str));//sử dụng hàm `std::stoul` để chuyển đổi chuỗi sang kiểu `unsigned long`, sau đó sử dụng ép kiểu để chuyển đổi sang kiểu `unsigned short`
+            tmp->K = n;
+
+            getline(ifs, str, ','); 
+            tmp->program = convertFromProgram(str);
+
+            getline(ifs, str, ',');
+            tmp->No = stoi(str);
+
+            getline(ifs, str, ',');
+            tmp->yearIn = stoi(str);
+
+
+            if (L_Class.head == nullptr) L_Class.head = L_Class.tail = new DLL<STAFF>;
+            else {
+                L_Class.tail->next = new DLL<STAFF>;
+                L_Class.tail->next->prev = L_Class.tail->next;
+                L_Class.tail = L_Class.tail->next;
+            }
+            L_Class.tail->data = tmp;
+            L_Class.tail->next = nullptr;
+         
+        }
+        break;
+    }
 }
 
 bool importStudentsInACourse(std::string filename) {
