f62e316da80f4dbd194b5ccf71727daab5c332dc
ketamean <truongthanhtoan2003@gmail.com>
2023-04-04 09:41:22 +0700

    Winform beginning

---
+++ TESTING WINFORM/CMS/Login.cpp
@@ -0,0 +1,16 @@
+#include "Login.h"
+#include "header.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<System::String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    importClasses();
+    importStudents();
+    CMS::Login form;
+    Application::Run(% form);
+}
+// 
